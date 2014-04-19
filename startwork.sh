4191202ae38b533197ef41d323074cf15ef53ff7
Jim Lind <spoon.vw@gmail.com>
2014-04-19 18:47:03 -0500

    A bunch of shell junk.
    The idea is that you can do the disk checking and what not
    easier/faster in bash.

--- startwork.sh
+++ startwork.sh
@@ -1,2 +1,31 @@
 #!/bin/sh
+
+mkdir /media/USB
+
+drive=$(find /sys/block/sd* | head -1)
+echo "$drive"
+
+echo ${drive##>*/}
+
+exit 0
+
+for drive in /sys/block/sd*
+do
+    if readlink $drive/device | grep -q usb
+    then
+        DEV=`basename $DEV`
+        echo "$DEV is a USB device, info:"
+        udevinfo --query=all --name $DEV
+        if [ -d /sys/block/${DEV}/${DEV}1 ]
+        then
+            echo "Has partitions " /sys/block/$DEV/$DEV[0-9]*
+        else
+            echo "Has no partitions"
+        fi
+        echo "No pants"
+    fi
+done
+
+exit 0
+
 python /home/pi/capture/work.py
