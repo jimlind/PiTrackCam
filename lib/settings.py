87ce13633ce0435d895bf88c68024393fec047d0
Jim Lind <spoon.vw@gmail.com>
2014-04-20 18:06:38 -0500

    Try using a universal tmp dir

--- lib/settings.py
+++ lib/settings.py
@@ -5,18 +5,22 @@ from os import system as cmd
 from uuid import uuid4
 
 import Image
+import tempfile as Temp
 
 class Settings:
 
     command_prefix = 'raspistill -sh 0 -co 0 -br 50 -sa 0 -ev 0 -ifx none -t 1000 -w 400 -h 400 -n'
-    storage = '/home/pi/capture/'
+    storage = '/tmp/'
+
+    def __init__():
+        self.storage = Temp.gettempdir()
 
     def get_auto(self):
         uuid = str(uuid4())
         file_name = self.storage + uuid + '.jpg'
         command = self.command_prefix + ' -o ' + file_name
         cmd(command)
-        
+
         im = Image.open(file_name)
         exif_data = im._getexif()
         auto_histogram = im.histogram()
@@ -39,4 +43,4 @@ class Settings:
         return {
             'iso': iso_value,
             'ss' : ss_value,
-	}
+        }
